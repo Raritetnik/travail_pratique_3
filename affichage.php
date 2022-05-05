fd876b09a050dd1baa854e30cf1c6fc57aa611a5
Raritetnik <101665571+Raritetnik@users.noreply.github.com>
2022-05-05 14:17:51 -0400

    Correction d'orthographe

--- affichage.php
+++ affichage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Traavail Practique 3 - Affichage</title>
+    <title>Travail Pratique 3 - Affichage</title>
      <style>
         table{
             max-width:100%;
